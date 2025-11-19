099c26f082cb3a3a55ea826a4f18b585f2192e48
Stefan <stefan@example.com>
2025-11-19 20:45:36 +0100

    fix(chart): register Chart.js Filler plugin to avoid fill warning

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 import { createApp } from 'vue'
 import App from './components/App.vue'
 
+// Polyfill for older browsers: ensure MediaQueryList supports addEventListener/removeEventListener
+if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
+  try {
+    const mql = window.matchMedia('(min-width: 0px)')
+    if (mql && typeof mql.addEventListener !== 'function' && typeof mql.addListener === 'function') {
+      const proto = Object.getPrototypeOf(mql)
+      if (proto) {
+        proto.addEventListener = function (type, listener) {
+          this.addListener(listener)
+        }
+        proto.removeEventListener = function (type, listener) {
+          this.removeListener(listener)
+        }
+      }
+    }
+  } catch (e) {
+    // ignore: matchMedia not available or unexpected environment
+  }
+}
+
 // Nextcloud Theme Integration
 import './theme.scss'
+
+// Register Chart.js 'Filler' plugin to avoid runtime warnings when using 'fill'
+import { Chart, Filler } from 'chart.js'
+Chart.register(Filler)
 
 createApp(App)
   .mount('#app')
