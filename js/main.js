299c8589860873fdbcd525a6873d6fa4b90151ef
Stefan <stefan@example.com>
2025-11-19 20:56:39 +0100

    Revert "fix(chart): register Chart.js Filler plugin to avoid fill warning"
    This reverts commit 47d2df69ddf3d1882c36db81b95ef49ecee68ead.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,6 @@
 import { createApp } from 'vue'
 import App from './components/App.vue'
 
-// Polyfill for older browsers: ensure MediaQueryList supports addEventListener/removeEventListener
-if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
-  try {
-    const mql = window.matchMedia('(min-width: 0px)')
-    if (mql && typeof mql.addEventListener !== 'function' && typeof mql.addListener === 'function') {
-      const proto = Object.getPrototypeOf(mql)
-      if (proto) {
-        proto.addEventListener = function (type, listener) {
-          this.addListener(listener)
-        }
-        proto.removeEventListener = function (type, listener) {
-          this.removeListener(listener)
-        }
-      }
-    }
-  } catch (e) {
-    // ignore: matchMedia not available or unexpected environment
-  }
-}
-
 // Nextcloud Theme Integration
 import './theme.scss'
 
